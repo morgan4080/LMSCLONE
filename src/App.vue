ccf00c73d64b226b27e08c03adbbe1bf6caf2e6c
Dickson <kibedickson3@gmail.com>
2023-05-30 11:22:28 +0300

    401 intercept

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,21 @@
 </template>
 
 <script lang="ts" setup>
-//
+import axios from "axios";
+
+axios.interceptors.response.use(undefined, function (err) {
+  return new Promise(function (resolve, reject) {
+    if (
+      err.response.status === 401 &&
+      err.config &&
+      !err.config.__isRetryRequest
+    ) {
+      const currentUrl = window.location.href;
+      window.location.href = `${
+        import.meta.env.VITE_APP_ROOT_AUTH
+      }?redirect_url=${currentUrl}`;
+    }
+    throw err;
+  });
+});
 </script>
