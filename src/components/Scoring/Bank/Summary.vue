75193f97f7cfededc2c1ec34461b3fcc8851fc91
Lenny Kibe <wlenny20@gmail.com>
2023-06-07 15:26:49 +0300

    Summary

---
+++ src/components/Scoring/Bank/Summary.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Cards from "@/components/Scoring/Bank/Summary/Cards.vue";
+import Overview from "@/components/Scoring/Bank/Summary/Overview.vue";
+</script>
+
+<template>
+  <Cards />
+  <Overview />
+</template>
