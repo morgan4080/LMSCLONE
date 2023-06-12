a47cdbb308dd6f5354d3f188c81bd6a38ed286ab
morgan4080 <morganmurungi@live.com>
2023-06-12 15:46:00 +0300

    ukimaliza rudisha redirect urls on navdrawer na main.ts

--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -630,8 +630,8 @@ const items = ref<
 
 const redirectAuth = () => {
   authStore.setAuthPrompt(false);
-  /*const currentUrl = window.location.href;
-  window.location.href = `${
+  const currentUrl = window.location.href;
+  /*window.location.href = `${
     import.meta.env.VITE_APP_ROOT
   }?redirect_url=${currentUrl}`;*/
 };
