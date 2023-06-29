4fd521c2e3c8bfb19bb4eccdf79fdffc24df5e2a
morgan4080 <morganmurungi@live.com>
2023-06-29 15:20:49 +0300

    stagingsc0

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker stop `docker ps | grep presta-lms | awk '{ print $1 }'` && docker rmi `docker image ls | grep presta-lms | awk '{ print $3 }'` -f && git pull && docker build --build-arg env=staging -t presta-lms/latest . && docker run -d -p 4080:80 presta-lms/latest
+CONTAINER_ID=docker ps | grep presta-lms | awk '{ print $1 }'
+IMAGE_ID=docker image ls | grep presta-lms | awk '{ print $3 }'
+
+docker stop "$CONTAINER_ID" &&
+ docker rmi "$IMAGE_ID" -f && git pull &&
+  docker build --build-arg env=staging -t presta-lms/latest . &&
+   docker run -d -p 4080:80 presta-lms/latest
 
